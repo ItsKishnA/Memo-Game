68035a96162f7bdc263b8b37d5578cf3bcae9a61
ItsKishnA <kishnayc@gmail.com>
2024-03-02 14:27:17 +0530

    Merged Tile Object

--- src/Files/Gameplot/gameplot.js
+++ src/Files/Gameplot/gameplot.js
@@ -1,9 +1,47 @@
-import { Text, View, StyleSheet, ToastAndroid } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  Pressable,
+  ToastAndroid,
+  Text,
+} from "react-native";
 import Tile from "../Tile/tile.js";
+import { useState } from "react";
 
-const GamePlot = () => {
-  let row = 2,
-    column = 4;
+// ARRAY DATA
+const CardImages = [
+  { image: require(`../../Images/Tiles/tile-back-cover.png`) },
+  { image: require(`../../Images/Tiles/1.png`) },
+  { image: require(`../../Images/Tiles/2.png`) },
+  { image: require(`../../Images/Tiles/3.png`) },
+  { image: require(`../../Images/Tiles/4.png`) },
+  { image: require(`../../Images/Tiles/5.png`) },
+  { image: require(`../../Images/Tiles/6.png`) },
+  { image: require(`../../Images/Tiles/7.png`) },
+  { image: require(`../../Images/Tiles/8.png`) },
+];
+
+const RenderedGamePlot = ({ row, column }) => {
+  const [choiceOne, setChoiceOne] = useState(false);
+  const [flipped, setFlipped] = useState(0);
+
+  // FUNCTION
+  const handleClick = (num) => {
+    choiceOne ? setFlipped(0) : setFlipped(num);
+    setChoiceOne(!choiceOne);
+    console.log("Pressed " + num);
+  };
+
+  // FUNCTION
+  const tile = () => {
+    return (
+      <View>
+        <Image source={CardImages[0].image} style={styles.tile} />
+      </View>
+    );
+  };
+
   return (
     <View style={styles.screen}>
       {Array(row)
@@ -13,10 +51,52 @@ const GamePlot = () => {
             {Array(column)
               .fill()
               .map((_, j) => (
-                <Tile key={j} keyValue={column * i + j + 1} />
+                <Pressable
+                  key={j}
+                  onPress={() => handleClick(column * i + j + 1)}
+                >
+                  {tile(j, (keyValue = column * i + j + 1))}
+                  {/* <Tile key={j} keyValue={column * i + j + 1} /> */}
+                </Pressable>
               ))}
           </View>
         ))}
+    </View>
+  );
+};
+
+const GamePlot = () => {
+  let row = 2;
+  let column = 4;
+
+  // {/*below */
+  // // Function to shuffle an array using Fisher-Yates algorithm
+  // function shuffle(array) {
+  //   for (let i = array.length - 1; i > 0; i--) {
+  //     const j = Math.floor(Math.random() * (i + 1));
+  //     [array[i], array[j]] = [array[j], array[i]];
+  //   }
+  //   return array;
+  // }
+
+  // // let originalArray = [1, 2, 3, 4, 5];
+
+  // // Create a new array by excluding the first element and then concatenating it with itself
+  // let newArray = CardImages.slice(1).map((obj) => ({ ...obj }));
+
+  // // Shuffle the elements of the new array
+  // // newArray = shuffle(newArray);
+
+  // // Map over the shuffled array to assign unique IDs to each element
+  // // newArray = newArray.map((element, index) => ({ id: index, value: element }));
+
+  // console.log(newArray);
+
+  // /*above */}
+  console.log("GamePlot.js");
+  return (
+    <View style={styles.screen}>
+      <RenderedGamePlot row={row} column={column} />
     </View>
   );
 };
@@ -29,8 +109,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   tile: {
-    width: 50,
-    height: 50,
+    width: 65,
+    height: 65,
     margin: 5,
   },
   eachLine: {
@@ -39,3 +119,14 @@ const styles = StyleSheet.create({
 });
 
 export default GamePlot;
+
+/*
+computing.png
+direct-memory-access.png
+grid.png
+micro-sd-card.png
+ram.png
+usb-stick.png
+hacker.png
+cloud.png
+*/
